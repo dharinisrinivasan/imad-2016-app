d9bfac233aa7d58ef804295ce3f116b37fcaab51
dharinisrinivasan <srinivasan.dharini@gmail.com>
2016-09-28 22:24:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,3 @@
-//console.log('Loaded!');
-
-//move the image
-//var img = document.getElementById('madi');
-//var marginLeft = 0;
-//function moveRight() {
-//    marginLeft = marginLeft + 10;
-//    img.style.marginLeft = marginLeft + 'px' ;
-//}
-//img.onclick = function () {
-//    var interval = setInterval(moveRight, 50);
-//};
 
 // counter code
 var counter = 0;
