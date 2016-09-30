add8c1127d3984d8cbaf09f61487d024a7f25ae6
dharinisrinivasan <srinivasan.dharini@gmail.com>
2016-09-30 22:49:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,23 @@
+// counter code
+
 var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function() {
-    counter = counter + 1;
-    var s = document.getElementById('count');
-    s.innerHTML = counter.toString();
+
+button.onClick = function() {
+    // Create a request Object to counter end point
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if(request.readystate === XMLHttpRequest.DONE){
+          if(request.status === 200) {
+              var counter = request.ResposeText;
+              var s = document.getElementById('count');
+              s.innerHTML=counter.toString();
+          }
+      }  
+      //Not done yet
+    };
     
+  // Make request
+  request.open('GET', 'http://dharinisrinivasan.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
